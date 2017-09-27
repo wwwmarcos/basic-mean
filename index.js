4480153d2b0ef0876183f06644b7792a1e8cb5db
Marcos Florencio <dstepbw@gmail.com>
2017-09-27 09:43:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,78 @@
-var bodyParser = require('body-parser')
-	,	http 	   	 = require('http') 
-	,	express    = require('express')
-	,	app 	     = express()
-	,	aluno 	   = require('./Schema.js');	
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const Aluno = require('./Schema.js')
+const path = require('path')
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({
-	extended: true
-}));
+  extended: true
+}))
 
-
-app.get('/', function (req, res){
-	res.sendFile(__dirname + '/webapp/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/webapp/index.html'))
 })
 
 app.post('/app/cadastrar', function (req, res) {
-	var newAluno = new aluno(
-		{ nome: req.body.nome, 
-			curso: req.body.curso
-		});
+  var newAluno = new Aluno({
+    nome: req.body.nome,
+    curso: req.body.curso
+  })
 
-	newAluno.save(function(err) {
-		if (err) throw err;
-	});
-	 
-	res.json({ message: 'Aluno recebido', data: req.body.nome});
+  newAluno.save(function (err) {
+    if (err) throw err
+  })
+
+  res.json({
+    message: 'Aluno recebido',
+    data: req.body.nome
+  })
 })
 
 app.get('/app/listarById/:id', function (req, res) {
-	aluno.findOne({ _id: req.params.id }, function(err, todos) {
-		if (err) throw err;
-		res.json(todos);
-	});
+  Aluno.findOne({
+    _id: req.params.id
+  }, function (err, todos) {
+    if (err) throw err
+    res.json(todos)
+  })
 })
 
 app.get('/app/listar', function (req, res) {
-	aluno.find(function(err, todos) {
-		if (err) throw err;
-		res.json(todos);
-	});
+  Aluno.find(function (err, todos) {
+    if (err) throw err
+    res.json(todos)
+  })
 })
 
 app.delete('/app/delete/:id', function (req, res) {
-	aluno.findOneAndRemove({ _id: req.params.id }, function(err) {
-		if (err) throw err;
-	});
-	res.json({ message: 'Aluno excluido'});
+  Aluno.findOneAndRemove({
+    _id: req.params.id
+  }, function (err) {
+    if (err) throw err
+  })
+  res.json({
+    message: 'Aluno excluido'
+  })
 })
 
-app.put("/app/editar", function(req, res){
-	aluno.findByIdAndUpdate(req.body._id, { nome: req.body.nome, curso: req.body.curso }, function(err, user) {
-		if (err) res.send(err);
-		res.json({ message: 'Aluno alterado'});
-	});
-});
+app.put('/app/editar', function (req, res) {
+  Aluno.findByIdAndUpdate(req.body._id, {
+    nome: req.body.nome,
+    curso: req.body.curso
+  }, function (err, user) {
+    if (err) res.send(err)
+    res.json({
+      message: 'Aluno alterado'
+    })
+  })
+})
 
-app.use(express.static(__dirname + '/webapp'));
+app.use(express.static(path.join(__dirname, '/webapp/index.html')))
 
-app.listen(3000)
-console.log("localhost:3000")
+app.set('port', (process.env.PORT || 3000))
+
+app.listen(app.get('port'), function () {
+  console.log('running on port', app.get('port'))
+})
